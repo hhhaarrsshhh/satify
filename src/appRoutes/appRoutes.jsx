8fc7280fcc01e4ca4a1e536de83dd782d9bf6936
harsh <saraswatharsh.997.com>
2025-08-05 19:07:44 +0530

    changes

--- src/appRoutes/appRoutes.jsx
+++ src/appRoutes/appRoutes.jsx
@@ -3,7 +3,7 @@ import Levels from "../components/Levels";
 import Profile from "../components/Profile";
 import Quiz from "../components/Quiz";
 import Home from "../components/Home";
-import QuizDetail from "../components/quizDetail";
+import QuizDetail from "../components/QuizDetail";
 export const routes = [
     { path: "/", element: <Home /> },
     { path: "/level", element: <Levels /> },
